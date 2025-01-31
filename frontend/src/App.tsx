776c20b46a443c9f4703559f50f8543309c62079
wolgan <danieljgjwolgan@gmail.com>
2025-01-31 12:46:40 -0500

    arreglamos cosas y ahora tenemos tailwind y shardcs y otras cosas mas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,11 +7,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+
+import { api } from "@/lib/api";
+
 function App() {
   const [totalspent, setTotalspent] = useState(0);
   useEffect(()=>{
     async function fetchTotal() {
-      const res=await fetch("/api/expenses/total-spent")
+      const res=await api.expenses["total-spent"].$get()
       const data=await res.json()
       setTotalspent(data.total)
     }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.dir, "./src"),
+      "@server": path.resolve(import.meta.dir, "../server"),
     },
   },
   server:{
